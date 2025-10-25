5aea15da59db34cdac322934de79fa891196a2f7
Yogesh chavan <yogeshchavan1209@gmail.com>
2025-10-25 10:57:57 +0530

    add cloudinary

--- Backend/config/cloudinary.js
+++ Backend/config/cloudinary.js
@@ -4,9 +4,9 @@ import dotenv from "dotenv";
 dotenv.config();
 
 cloudinary.config({
-  cloud_name: process.env.COLUDINARY_CLOUD_NAME,
-  api_key: process.env.COLUDINARY_API_KEY,
-  api_secret: process.env.COLUDINARY_API_SECRETE,
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
 export default cloudinary;
