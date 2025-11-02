35b08bb876ed95954726ba3699f7e694348fdce3
Yogesh chavan <yogeshchavan1209@gmail.com>
2025-11-03 00:03:12 +0530

    fix import statement

--- frontend/src/components/studentLoginDashboard.jsx
+++ frontend/src/components/studentLoginDashboard.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
-import TopBar from "./TopBar";
+import TopBar from "../Topbar";
 // -------------------------------------------------
 //  ICONS  (same heart / comment you already had)
 // -------------------------------------------------
